71291a488ad0d71516925ff8d48fc91bf20c50af
Mark Carthon <macarthoniii@gmail.com>
2023-05-30 17:18:41 -0400

    learning how to use openai api in python

--- openai-practise.py
+++ openai-practise.py
@@ -1,11 +1,23 @@
-mport openai
+import openai
 import credentials
 # openai.organization = "org-rjk9KVuSZRAXwdBUkubDvur0"
 openai.api_key = credentials.openai_api_key
 # model_list = sorted([openai.Model.list()["data"][i].id for i in range(len(openai.Model.list()["data"]))])
 # print(model_list)
-prompt = "ask me a good question"
+# prompt = "ask me a good question"
 
-print(openai.Completion.create(
-    model = "text-davinci-003",
-    prompt = prompt)["choices"][0].text)
+# print(openai.Completion.create(
+#     model = "text-davinci-003",
+#     prompt = prompt)["choices"][0].text)
+
+print(
+    openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo-0301",
+        messages = [
+            {
+                "role": "user",
+                "content": "Who originally wrote the song O Holy Night?"
+            }
+        ]
+    )["choices"][0].message.content
+)
