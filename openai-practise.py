86e1aa5a792338d7e1acef71999552ebbf535997
Mark Carthon <macarthoniii@gmail.com>
2023-05-30 18:34:47 -0400

    added langchain to extend chatgpt

--- openai-practise.py
+++ openai-practise.py
@@ -1,6 +1,5 @@
 import openai
 import credentials
-# openai.organization = "org-rjk9KVuSZRAXwdBUkubDvur0"
 openai.api_key = credentials.openai_api_key
 # model_list = sorted([openai.Model.list()["data"][i].id for i in range(len(openai.Model.list()["data"]))])
 # print(model_list)
@@ -10,14 +9,31 @@ openai.api_key = credentials.openai_api_key
 #     model = "text-davinci-003",
 #     prompt = prompt)["choices"][0].text)
 
-print(
-    openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo-0301",
-        messages = [
-            {
-                "role": "user",
-                "content": "Who originally wrote the song O Holy Night?"
-            }
-        ]
-    )["choices"][0].message.content
+# print("\n" +
+#     openai.ChatCompletion.create(
+#         model = "gpt-3.5-turbo-0301",
+#         messages = [
+#             {
+#                 "role": "user",
+#                 "content": "Do you have access to LinkedIn posts or content?"
+#             }
+#         ]
+#     )["choices"][0].message.content
+# + "\n")
+
+import langchain
+
+llm = langchain.llms.OpenAI(openai_api_key = credentials.openai_api_key)
+
+tools = langchain.agents.load_tools(["google-search"], llm = llm)
+
+agent = langchain.agents.initialize_agent(
+    tools,
+    llm,
+    agent = langchain.agents.AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    verbose = True
 )
+
+prompt = "what does anomaly mean?"
+
+agent.run(f"{prompt}")
